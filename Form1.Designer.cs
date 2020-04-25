00983010e574c735f9c0868b311119c7fe6465ba
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-04-25 10:12:48 +0530

    Basic Drawing app - Rectangle

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace MyPaint_CSharp
+{
+    partial class frmMyPaint
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.canvas = new System.Windows.Forms.Panel();
+            this.btnRectangle = new System.Windows.Forms.Button();
+            this.lblFooter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.Color.White;
+            this.canvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.canvas.Location = new System.Drawing.Point(132, 12);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(656, 426);
+            this.canvas.TabIndex = 0;
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint);
+            this.canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseDown);
+            this.canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseMove);
+            this.canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseUp);
+            // 
+            // btnRectangle
+            // 
+            this.btnRectangle.Location = new System.Drawing.Point(12, 12);
+            this.btnRectangle.Name = "btnRectangle";
+            this.btnRectangle.Size = new System.Drawing.Size(75, 23);
+            this.btnRectangle.TabIndex = 1;
+            this.btnRectangle.Text = "Rectangle";
+            this.btnRectangle.UseVisualStyleBackColor = true;
+            this.btnRectangle.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblFooter
+            // 
+            this.lblFooter.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblFooter.BackColor = System.Drawing.Color.Black;
+            this.lblFooter.ForeColor = System.Drawing.Color.White;
+            this.lblFooter.Location = new System.Drawing.Point(0, 462);
+            this.lblFooter.Name = "lblFooter";
+            this.lblFooter.Size = new System.Drawing.Size(817, 23);
+            this.lblFooter.TabIndex = 2;
+            // 
+            // frmMyPaint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 482);
+            this.Controls.Add(this.lblFooter);
+            this.Controls.Add(this.btnRectangle);
+            this.Controls.Add(this.canvas);
+            this.Name = "frmMyPaint";
+            this.Text = "MyPaint";
+            this.Load += new System.EventHandler(this.frmMyPaint_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel canvas;
+        private System.Windows.Forms.Button btnRectangle;
+        private System.Windows.Forms.Label lblFooter;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyPaint_CSharp
+{
+    public partial class frmMyPaint : Form
+    {
+        string shape = "rectangle";
+        bool startPaint = false;
+        int x = 0;
+        int y = 0;
+        int ex = 0;
+        int ey = 0;
+
+        Graphics g;
+
+        public frmMyPaint()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+
+            g = canvas.CreateGraphics();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            shape = "rectangle";
+        }
+
+        private void canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            startPaint = true;
+            this.Text =   "Mouse Down at : " + e.X.ToString() + ", " + e.Y.ToString();
+            if (shape == "rectangle")
+            {
+                SolidBrush sb = new SolidBrush(Color.Yellow);
+                x = e.X;
+                y = e.Y;
+
+            }
+        }
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            lblFooter.Text = "Mouse Move at : " + e.X.ToString() + ", " + e.Y.ToString();
+
+            if (startPaint && shape == "rectangle")
+            {
+                ex = e.X;
+                ey = e.Y;
+
+                canvas.Invalidate();
+            } else
+            {
+                // Draw Line
+                // Set Pen Back color and Line Width
+                /*Pen p = new Pen(new SolidBrush(Color.Red), 2);
+
+                // Draw line
+                g.DrawLine(p, new Point(x ?? e.X, y ?? e.Y), new Point(e.X, e.Y));
+                x = e.X;
+                y = e.Y;*/
+            }
+        }
+
+        private void canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            startPaint = false;
+            x = -1;
+            y = -1;
+            shape = "";
+        }
+
+        private void canvas_Paint(object sender, PaintEventArgs e)
+        {
+            g.Clear(Color.White);
+            SolidBrush sb = new SolidBrush(Color.Yellow);
+            g.FillRectangle(sb, x, y, ex - x, ey - y);
+        }
+
+        private void frmMyPaint_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
